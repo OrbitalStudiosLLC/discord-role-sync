ef69e626daf35b74027b6b6500a6b074bed5b2fe
Crispy <34707863+JackCrispy@users.noreply.github.com>
2022-03-10 12:17:03 +1100

    init :fire: :sunflower: :horse:

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # discord-role-sync
-Sync roles between multiple discord servers  
+Sync roles between multiple discord servers.
+
+## Config
+In config.json, you will find a roles array. For every role you want to sync between servers, you should create a new object in that array, the schema for that object is as follows;
+```js
+    {
+        "base_guild": "805298672475701249", // The "base" Guild ID, where the original role is.
+        "base_role_id": "947745665335840779", // The "base" role ID. (Syncing Role)
+        "extending_guild": "767525765490278430", // The Guild ID you want to sync this role to
+        "extending_role_id": "951270256217841744" // The role ID of the synced role on the extending guild
+    },
+```
+
+Any member in the "base" role will be added to the "extending" role in the other guild.
+
+## Options
+This check runs whenever the bot starts, it also runs on a cron timer which you can edit in the config. Whenever a member joins any guild, it checks all roles again to instantly sync the member's roles.
+
+### This app is used in production and there for no prs will be accepted on master, please pr to dev for it to be reviewed.
---
+++ index.js
@@ -0,0 +1,66 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS] });
+const config = require('./config.json');
+const cron = require('node-cron');
+require('dotenv').config()
+
+client.on('ready', async () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    await syncRoles();
+});
+
+client.on('guildMemberAdd', async member => {
+    await progressiveSearch(member.id);
+});
+
+cron.schedule(config.cron_time, () => {
+    syncRoles();
+});
+
+async function progressiveSearch(member_id) {
+    try {
+        for(i = 0; i < config.roles.length; i++){
+            let base_guild = client.guilds.cache.get(config.roles[i].base_guild);
+            let base_member = base_guild.members.cache.find(member => member.id === member_id);
+            if (base_member) {
+                let target_guild = client.guilds.cache.get(config.roles[i].extending_guild);
+                let target_role = target_guild.roles.cache.find(role => role.id === config.roles[i].extending_role_id);
+                let target_member = target_guild.members.cache.find(member => member.id === member_id);
+                if (target_member) {
+                    target_member.roles.add(target_role);
+                }
+            }
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function syncRoles() {
+    try {
+        for(i = 0; i < config.roles.length; i++){
+            let base_guild = client.guilds.cache.get(config.roles[i].base_guild);
+
+            let target_guild = client.guilds.cache.get(config.roles[i].extending_guild);
+            let target_role = target_guild.roles.cache.find(role => role.id === config.roles[i].extending_role_id);
+        
+            let base_members = await base_guild.members.fetch();
+            let target_members = await target_guild.members.fetch();
+            
+            base_members = Array.from(base_members.values());
+            target_members = Array.from(target_members.values());
+
+            for (j = 0; j < base_members.length; j++){
+                let base_member = base_members[j];
+                let target_member = target_members.find(member => member.id === base_member.id);
+                if (target_member) {
+                    target_member.roles.add(target_role);
+                }
+            }
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+client.login(process.env.CLIENT_TOKEN);
